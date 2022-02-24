298284060b08376587c0df14ab943ab7360c2c15
Tio Renndy <tio@kata.ai>
2022-02-24 14:30:28 +0700

    fix/change app port

--- fetch-app/configs/server.go
+++ fetch-app/configs/server.go
@@ -7,15 +7,8 @@ import (
 
 // GetAddress will return formatted address
 func GetAddress() string {
-	host := ""
-	if os.Getenv("APP_HOST") != "" {
-		host = os.Getenv("APP_HOST")
-	}
-
-	app_port := "8081"
-	if os.Getenv("APP_PORT") != "" {
-		app_port = os.Getenv("APP_PORT")
-	}
+	host := os.Getenv("APP_HOST")
+	app_port := os.Getenv("APP_PORT")
 
 	return fmt.Sprintf("%s:%s", host, app_port)
 }
